0605502e4ae598b7f28e1fe95533b86169871e9f
Gabibrj18 <102430506+Gabibrj18@users.noreply.github.com>
2026-04-28 17:27:56 +0300

    Update agent_apb

--- Testing Environment/agent_apb.sv
+++ Testing Environment/agent_apb.sv
@@ -15,16 +15,11 @@ class agent_apb extends uvm_agent;
 
   `uvm_component_utils(agent_apb)
 
-  // componentele agentului
   driver_agent_apb                driver_agent_apb_inst0;
   monitor_apb                     monitor_apb_inst0;
   uvm_sequencer #(tranzactie_apb) sequencer_agent_apb_inst0;
 
-  // port de iesire din agent
   uvm_analysis_port #(tranzactie_apb) de_la_monitor_apb;
-
-  // agent activ / pasiv
-  local int is_active = 1;
 
   function new(string name = "agent_apb", uvm_component parent = null);
     super.new(name, parent);
@@ -34,24 +29,25 @@ class agent_apb extends uvm_agent;
   virtual function void build_phase(uvm_phase phase);
     super.build_phase(phase);
 
-    // monitorul exista si la agent activ si la agent pasiv
     monitor_apb_inst0 = monitor_apb::type_id::create("monitor_apb_inst0", this);
 
-    if (is_active == 1) begin
-      sequencer_agent_apb_inst0 = uvm_sequencer#(tranzactie_apb)::type_id::create("sequencer_agent_apb_inst0", this);
-      driver_agent_apb_inst0    = driver_agent_apb::type_id::create("driver_agent_apb_inst0", this);
+    // get_is_active() citeste parametrul UVM_ACTIVE/UVM_PASSIVE setat din exterior
+    if (get_is_active() == UVM_ACTIVE) begin
+      sequencer_agent_apb_inst0 =
+        uvm_sequencer#(tranzactie_apb)::type_id::create("sequencer_agent_apb_inst0", this);
+      driver_agent_apb_inst0 =
+        driver_agent_apb::type_id::create("driver_agent_apb_inst0", this);
     end
   endfunction
 
   virtual function void connect_phase(uvm_phase phase);
     super.connect_phase(phase);
 
-    // monitor -> agent
     monitor_apb_inst0.port_date_monitor_apb.connect(de_la_monitor_apb);
 
-    // sequencer -> driver
-    if (is_active == 1) begin
-      driver_agent_apb_inst0.seq_item_port.connect(sequencer_agent_apb_inst0.seq_item_export);
+    if (get_is_active() == UVM_ACTIVE) begin
+      driver_agent_apb_inst0.seq_item_port.connect(
+        sequencer_agent_apb_inst0.seq_item_export);
     end
   endfunction
 
